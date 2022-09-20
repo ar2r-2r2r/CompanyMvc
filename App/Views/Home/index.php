9c501de079e0ed8054b23a41a2d696c7de92c895
Ar2r <roooob66666666666@gmail.com>
2022-09-20 14:07:36 +0300

    query params

--- App/Views/Home/index.php
+++ App/Views/Home/index.php
@@ -11,23 +11,23 @@
 <table>
     <tr>
         <th>
-            <a href="/php-mvc/public/sortAscByFN">Ʌ</a>
-            <a href="/php-mvc/public/sortDescByFN">V</a>
+            <a href="/php-mvc/public/index.php?sortDescByS">Ʌ</a>
+            <a href="/php-mvc/public/index.php?sortDescByFN">V</a>
             first name
         </th>
         <th>
-            <a href="/php-mvc/public/sortAscByLN">Ʌ</a>
-            <a href="/php-mvc/public/sortDescByLN">V</a>
+            <a href="/php-mvc/public/index.php?sortAscByLN">Ʌ</a>
+            <a href="/php-mvc/public/index.php?sortDescByLN">V</a>
             last name
         </th>
         <th>
-            <a href="/php-mvc/public/sortAscByDOB">Ʌ</a>
-            <a href="/php-mvc/public/sortDescByDOB">V</a>
+            <a href="/php-mvc/public/index.php?sortAscByDOB">Ʌ</a>
+            <a href="/php-mvc/public/index.php?sortDescByDOB">V</a>
             date of birth
         </th>
         <th>
-            <a href="/php-mvc/public/sortAscByS">Ʌ</a>
-            <a href="/php-mvc/public/sortDescByS">V</a>
+            <a href="/php-mvc/public/index.php?sortAscByS">Ʌ</a>
+            <a href="/php-mvc/public/index.php?sortDescByS">V</a>
             salary
         </th>
     </tr>
